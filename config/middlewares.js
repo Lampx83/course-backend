23221d28a0f938b5b51187142030ffc11352042b
duyviet2101 <whoisduyviet@gmail.com>
2024-06-15 17:20:43 +0700

    config CORS & CSP

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,11 +2,30 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:63342', 'https://fit-frontend-3y56.vercel.app', 'https://fit.neu.edu.vn'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http://101.96.66.219:8015', 'https://fit.neu.edu.vn'],
+        }
+      }
+    }
+  }
 ];
