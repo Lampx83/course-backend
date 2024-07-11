9eadd0ade50a380a5f49fd3ea190e32fcddda57a
duyviet2101 <whoisduyviet@gmail.com>
2024-07-11 15:52:41 +0700

    contact infor API

---
+++ src/api/contact-infor/services/contact-infor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-infor service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-infor.contact-infor');
