1791720634c936a274ec0b5c3eb99f5b0da82ad1
duyviet2101 <whoisduyviet@gmail.com>
2024-11-29 23:17:01 +0700

    add edtech-lab single type

---
+++ src/api/edtech-lab/routes/edtech-lab.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edtech-lab router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::edtech-lab.edtech-lab');
---
+++ src/api/edtech-lab/services/edtech-lab.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * edtech-lab service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::edtech-lab.edtech-lab');
